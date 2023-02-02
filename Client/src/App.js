e623ee3314af4ab6aa382b4334be3910bd4332bb
VuongHuy <96243959+VuongHuy2k1@users.noreply.github.com>
2023-02-02 10:26:04 +0700

    add file

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <AudioPlayer
+          autoPlay
+           src="https://www.youtube.com/watch?v=sqpg1qzJCGQ&t=463s
+           "
+           onPlay={e => console.log("onPlay")}
+  
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
